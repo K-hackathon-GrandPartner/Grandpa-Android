da880e659013ac6ebf656fc3511a92944dd89b33
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2023-08-11 22:51:08 +0900

    필터 레이아웃 만드는 중

---
+++ app/src/main/java/com/example/grandpa/FilterActivity.kt
@@ -0,0 +1,10 @@
+package com.example.grandpa
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.filter_roominfo) // filter_roomInfo.xml 파일과 연결
+    }
+}
