cbed34a39c801e2a9f2033fada90a05602501bbf
HeeYoung <hykim02@gnu.ac.kr>
2023-08-13 02:35:28 +0900

    filtering 레이아웃 제작 완료

--- app/src/main/java/com/example/grandpa/FilterActivity.kt
+++ app/src/main/java/com/example/grandpa/FilterActivity.kt
@@ -1,10 +1,15 @@
 package com.example.grandpa
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class FilterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.filter_roominfo) // filter_roomInfo.xml 파일과 연결
+
+//        val sumOfRoom = findViewById<TextView>(R.id.CountRoom)
+//
+//        sumOfRoom.text = "총 ${ShowRoomActivity.} 개"
     }
 }
