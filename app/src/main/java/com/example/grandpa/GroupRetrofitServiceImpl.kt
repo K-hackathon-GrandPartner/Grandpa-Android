6f65e146e308a1f45d25ae4a5579110f1f3e74a0
HeeYoung <hykim02@gnu.ac.kr>
2023-08-26 23:45:36 +0900

    -내부저장소 만들어서 필터링 값 저장 진행중 -레이아웃 하단바 이미지 다시 설정 -아이템뷰 레이아웃 재조정 필요

--- app/src/main/java/com/example/grandpa/GroupRetrofitServiceImpl.kt
+++ app/src/main/java/com/example/grandpa/GroupRetrofitServiceImpl.kt
@@ -5,8 +5,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 //room 모두 가져오는 싱글톤 패턴 object 구현
 object ShowRoomImpl {
-    //const val BASE_URL = "http://dongho18.iptime.org:3000/api/room/" //여기 주소 무조건 /로 끝나야 함
-    const val BASE_URL = "http://35.216.98.240:3000/api/room/"
+     //여기 주소 무조건 /로 끝나야 함
+    //서버주소 해킹 방지 위해 ignore처리 필요
+    const val BASE_URL = "http://35.216.120.52:3000/api/room/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
@@ -18,8 +19,8 @@ object ShowRoomImpl {
 
 //room의 해당 id만 가져오는 object
 object DetailRoomImpl{
-    //const val BASE_URL = "http://dongho18.iptime.org:3000/api/room/"
-    const val BASE_URL = "http://35.216.98.240:3000/api/room/"
+    const val BASE_URL = "http://35.216.120.52:3000/api/room/"
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(ShowRoomImpl.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
